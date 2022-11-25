10dfc72d2f363096433cdae432b63404e9a26a1b
Minchang Sung <53217819+tjdalsckd@users.noreply.github.com>
2022-11-25 16:36:22 +0900

    xenomai_rt_test
    test

---
+++ src/main.cpp
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <native/task.h>
+#include <native/timer.h>
+#include <rtdk.h>
+#include <iostream>
+using namespace std;
+
+#include <rtdk.h>		//The rdtk real-time printing library
+
+#include <alchemy/task.h>
+
+RT_TASK hello_task;
+
+// function to be executed by task
+void helloWorld(void *arg)
+{
+  RT_TASK_INFO curtaskinfo;
+
+  printf("Hello World!\n");
+
+  // inquire current task
+  rt_task_inquire(NULL,&curtaskinfo);
+
+  // print task name
+  printf("Task name : %s \n", curtaskinfo.name);
+}
+
+int main(int argc, char* argv[])
+{
+  char  str[10];
+
+  printf("start task\n");
+  sprintf(str,"hello");
+
+  /* Create task
+   * Arguments: &task,
+   *            name,
+   *            stack size (0=default),
+   *            priority,
+   *            mode (FPU, start suspended, ...)
+   */
+  rt_task_create(&hello_task, str, 0, 50, 0);
+
+  /*  Start task
+   * Arguments: &task,
+   *            task function,
+   *            function argument
+   */
+  rt_task_start(&hello_task, &helloWorld, 0);
+}
